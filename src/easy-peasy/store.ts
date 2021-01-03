704aa8d6971dea705668fd169c27d3037a78a781
Pasha Brovarnik <pasha@Pashas-MacBook-Pro.local>
2021-01-03 14:02:04 -0800

    fixed date get month function

--- src/easy-peasy/store.ts
+++ src/easy-peasy/store.ts
@@ -71,7 +71,7 @@ export const store = createStore<Model>({
 		const { fetchData, setTmdbDiscoverData } = actions;
 		const formatDate = (date: Date) => {
 			const day = date.getDate();
-			const month = date.getMonth();
+			const month = date.getMonth() + 1;
 			return `${date.getFullYear()}-${month < 10 ? '0' + month : month}-${
 				day < 10 ? '0' + day : day
 			}`;
